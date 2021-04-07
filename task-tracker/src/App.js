a41b52307fbbce8cb1765f5d364289369fa3147a
My Tran <myytran28@gmail.com>
2021-04-07 12:58:32 -0400

    add tasks to App.js

--- task-tracker/src/App.js
+++ task-tracker/src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
 
-function App() {
+const App = ()  => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Doctors Appointment',
+      day: 'Feb 5th at 2:30pm',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Meeting at School',
+      day: 'Feb 8th at 12:30pm',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'Gym',
+      day: 'Feb 5th at 2:30pm',
+      reminder: false,
+    },
+    {
+      id: 4,
+      text: 'DryClean',
+      day: 'Feb 6th at 10:30pm',
+      reminder: true,
+    },
+    {
+      id: 5,
+      text: 'Dinner at Home',
+      day: 'Feb 9th at 6:30pm',
+      reminder: true,
+    }
+  ])
+
+  const deleteTask = (id) => {
+    //show list of tasks whose ids don't match the one that is clicked.
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} />) : ('No Tasks to Show')}
     </div>
   );
 }
